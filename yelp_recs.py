8025439cf6a774e70a7cc31b6609f008f6e17d5c
cjmurphy718 <cjp449@stern.nyu.edu>
2022-08-09 19:18:37 -0400

    Added business reviews

--- yelp_recs.py
+++ yelp_recs.py
@@ -13,15 +13,29 @@ API_KEY = os.getenv("API_KEY")
 
 request_url = "https://api.yelp.com/v3/businesses/search"
 request_params = {
-    'term': 'breakfast', 
-    'limit': 50, 
-    'offset': 50, 
-    'price': ['1', "2"], 
-    'radius': 10000, 
-    'categories': ['chinese', 'pizza'], # FYI looks like when we do a search for businesses with categories ['chinese', 'pizza'] it seems to return either chinese OR pizza. Not restaurants that are a fusion / combination of both
-    'location': '20002'
+    'term': 'dinner', 
+    'limit': 2, 
+    #'offset': 50, 
+    #'price': ['1', "2"], 
+    'radius': 1000, 
+    'categories': ['pizza'], # FYI looks like when we do a search for businesses with categories ['chinese', 'pizza'] it seems to return either chinese OR pizza. Not restaurants that are a fusion / combination of both
+    'location': 'New York'
 }
 request_headers = {'Authorization': f"bearer {API_KEY}"}
 response = requests.get(url=request_url, params=request_params, headers=request_headers)
-parsed_response = json.loads(response.text)
-pprint(parsed_response)
+parsed = json.loads(response.text)
+
+businesses = parsed["businesses"]
+for business in businesses:
+    print("Name:", business["name"])
+    print("Rating:", business["rating"])
+    print("Address:", " ".join(business["location"]["display_address"]))
+    print("Phone:", business["phone"])
+    print("\n")
+
+##Import Business Reviews
+Business_ID = business["id"]
+reviews_url = f"https://api.yelp.com/v3/businesses/{Business_ID}/reviews"
+req = requests.get(url=reviews_url, headers=request_headers)
+json.loads(req.text)
+pprint(req.text)
