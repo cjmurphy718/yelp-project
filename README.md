f35bf0826fef703a99138b848f6357b6f4ad0b3d
cjmurphy718 <cjp449@stern.nyu.edu>
2022-08-09 18:22:54 -0400

    Attempted pulling in Yelp API

--- README.md
+++ README.md
@@ -18,5 +18,12 @@ conda activate yelp-env
 
 Install requirements
 ```sh
-pip install yelp
+pip install yelpapi
 ```
+
+Run the code
+```sh
+python yelp_recs.py
+```
+
+Input your Client ID and hit enter, followed by your API Key and hit enter - You will not see the text.
--- yelp_recs.py
+++ yelp_recs.py
@@ -1,11 +1,25 @@
 import requests
 import json
+from getpass import getpass
+import os
+from dotenv import load_dotenv
+from pprint import pprint
+
+CLIENT_ID = getpass("CLIENT_ID:")
+API_KEY = getpass("API_KEY:")
+
+
 yelp_api = "https://api.yelp.com/v3/"
-
-
-x = requests.get(yelp_api)
-print(dir(x))
-
-data = json.loads(x.text)
-print(type(data))
-print(data)
+request_params = {
+    'term': 'breakfast', 
+    'limit': 50, 
+    'offset': 50, 
+    'price': ['1', "2"], 
+    'radius': 10000, 
+    'categories': ['chinese', 'pizza'], # FYI looks like when we do a search for businesses with categories ['chinese', 'pizza'] it seems to return either chinese OR pizza. Not restaurants that are a fusion / combination of both
+    'location': '20002'
+}
+request_headers = {'Authorization': f"bearer {API_KEY}"}
+response = requests.get(url=yelp_api, params=request_params, headers=request_headers)
+parsed_response = json.loads(response.text)
+pprint(parsed_response)
