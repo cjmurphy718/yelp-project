c486cfca385898ba9a4721bbcbb41392a412e7fa
cjmurphy718 <cjp449@stern.nyu.edu>
2022-08-11 00:45:33 -0400

    Updated python file to define functions

--- app/yelp_recs.py
+++ app/yelp_recs.py
@@ -1,69 +1,81 @@
-
-import json
-import requests
 import os
+import json
 from pprint import pprint
+import requests
 import random
-from IPython.display import Image, display
 from dotenv import load_dotenv
+#from app import APP_ENV
+
 
 load_dotenv()
-
+APP_ENV = os.getenv("APP_ENV", default="production") # use "production" on a remote server
 API_KEY = os.getenv("API_KEY")
-if __name__ == "__main__":
-    valid_choices = ("pizza", "mexican", "italian", "mexican", "sushi", "american", "asian", "thai", "indian", "mediterannean", "greek", "japanese", "noodles", "dim sum", "seafood")
-    ##Get User Inputs
-    location = input("What is your location? ")
-    craving = input("Do you have a craving? (Yes/No) ")
-    craving = craving.upper()
-    if craving == "YES":
-        category = input("What are you craving? ")
+ZIP_CODE = 10001
+PRICE_MEDIAN = 2
+RADIUS_DEFAULT = 1000
+valid_choices = ("pizza", "mexican", "italian", "mexican", "sushi", "american", "asian", "thai", "indian", "mediterannean", "greek", "japanese", "noodles", "dim sum", "seafood")
+conMil = 1609.34
+
+def get_location():
+    if APP_ENV=="production":
+        user_zip = input("What is your 5-digit zip code? ")
     else:
-        category = random.choice(valid_choices)
-    miles = input("How many miles are you willing to go? ")
-    miles = int(miles)
-    conMil = 1609.34
-    radius = miles * conMil
-    radius = int(radius)
-    #print(radius)
-    price = input("What price do you want to pay on a scale of 1-4, 4 being the most expensive? ")
-    if price > "4":
-        print("This is not a valid option! ")
-        quit()
-    # COME BACK TO THIS - get it to loop
+        user_zip = ZIP_CODE
+    return user_zip
+def get_price():
+    if APP_ENV=="production":
+        price_limit = input("What price do you want to pay on a scale of 1-4, 4 being the most expensive? ")
+    else:
+        price_limit = PRICE_MEDIAN
+    return price_limit
+def get_radius():
+    if APP_ENV=="production":
+        radius_limit = input("How many miles are you willing to go? ")
+        radius_limit = int(radius_limit)
+        radius_limit = radius_limit * conMil
+        radius_limit = int(radius_limit)
+    else:
+        radius_limit = RADIUS_DEFAULT
+    return radius_limit   
+def get_category():
+    if APP_ENV=="production":
+        category_choice = input("Are you craving anything? ")
+    else:
+        category_choice = random.choice(valid_choices)
+    return category_choice    
+def get_yelp_recs():
     request_url = "https://api.yelp.com/v3/businesses/search"
     request_params = {
-        'term': 'dinner', 
+       'term': 'dinner', 
         'limit': 1, 
-        'price': price,
-        'radius': radius, 
-        'categories': category, # FYI looks like when we do a search for businesses with categories ['chinese', 'pizza'] it seems to return either chinese OR pizza. Not restaurants that are a fusion / combination of both
-        'location': location,
+        'price': price_limit,
+        'radius': radius_limit, 
+        'categories': category_choice, # FYI looks like when we do a search for businesses with categories ['chinese', 'pizza'] it seems to return either chinese OR pizza. Not restaurants that are a fusion / combination of both
+        'location': user_zip
     }
     request_headers = {'Authorization': f"bearer {API_KEY}"}
     response = requests.get(url=request_url, params=request_params, headers=request_headers)
     parsed = json.loads(response.text)
     #pprint(parsed)
-
-    image_url = "https://api.yelp.com/v3/businesses/{id}}"
     businesses = parsed["businesses"]
+   # business_result = []
     for business in businesses:
         print("Name:", business["name"])
         print("Rating:", business["rating"])
         print("Address:", " ".join(business["location"]["display_address"]))
         print("Phone:", business["phone"])
-        # print(business["photos"])
-    # image_url = business["image_url"]
-        #display(Image(url=image_url, height=100))
-        #print("\n")
+       # business_result.append({
+        #business_name = business["name"]
+       # "Rating": business["rating"],
+       # "Address": " ".join(business["location"]["display_address"]),
+       # "Phone": business["phone"]
+    return
+if __name__ == "__main__":
+    user_zip=get_location()
+    price_limit=get_price()
+    radius_limit=get_radius()
+    category_choice=get_category()
+    result = get_yelp_recs()
+    #print(get_yelp_recs())
 
-    ## Import Business Reviews
-    business_id = business["id"]
-    reviews_url = f"https://api.yelp.com/v3/businesses/{business_id}/reviews"
-    req = requests.get(url=reviews_url, headers=request_headers)
-    parsed_reviews = json.loads(req.text)
 
-    print("What have recent visitors said about", business["name"], "...")
-    for review in parsed_reviews["reviews"]:
-        print("Rating: ", review["rating"])
-        print(review["text"])
--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,50 +1,18 @@
-# web_app/routes/weather_routes.py
+# web_app/routes/home_routes.py
 
-from flask import Blueprint, request, jsonify, render_template, redirect, flash
+from flask import Blueprint, request, render_template
 
-#from app.yelp_recs import get_hourly_forecasts
+home_routes = Blueprint("home_routes", __name__)
 
-home_routes = Blueprint("yelp_recs", __name__)
+# /home routes
+@home_routes.route("/")
+@home_routes.route("/home")
+def index():
+    print("HOME...")
+    return render_template("home.html")
 
-#@weather_routes.route("/weather/form")
-#def weather_form():
-#    print("WEATHER FORM...")
- #   return render_template("weather_form.html")
-
-#@weather_routes.route("/weather/forecast", methods=["POST"])
-#def weather_forecast():
- #   print("WEATHER FORECAST...")
-#
- #   request_data = dict(request.form)
-  #  print("FORM DATA:", request_data)
-#####results = get_hourly_forecasts(country_code=country_code, zip_code=zip_code)
-    #if results:
-     #   #flash("Weather Forecast Generated Successfully!", "success")
-      #  return render_template("weather_forecast.html",
-       #     country_code=country_code,
-        #    zip_code=zip_code,
-         #   results=results
-        #)
-    #else:
-     #   #flash("Geography Error. Please try again!", "danger")
-      #  return redirect("/weather/form")
-
-#
-# API ROUTES
-#
-
-#@weather_routes.route("/api/weather/forecast.json")
-#def weather_forecast_api():
-#    print("WEATHER FORECAST (API)...")
-
-#    url_params = dict(request.args)
- #   print("URL PARAMS:", url_params)
-
-  #  country_code = url_params.get("country_code") or "US"
-   # zip_code = url_params.get("zip_code") or "20057"
-
-#    results = get_hourly_forecasts(country_code=country_code, zip_code=zip_code)
- #   if results:
-  #      return jsonify(results)
-   # else:
-    #    return jsonify({"message":"Invalid Geography. Please try again."}), 404
+# /form routes
+@home_routes.route("/yelp/form")
+def about():
+    print("WHERE TO TONIGHT?!...")
+    return render_template("yelp_form.html")
